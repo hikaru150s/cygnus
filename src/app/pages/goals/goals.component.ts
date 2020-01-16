e0ec6ee2cc7069c52e49686f0667a262a78a826e
hikaru150s <haikaladha1996@gmail.com>
2020-01-16 17:48:08 +0700

    goal description fix

--- src/app/pages/goals/goals.component.ts
+++ src/app/pages/goals/goals.component.ts
@@ -46,6 +46,10 @@ export class GoalsComponent implements OnInit {
         title: 'Name',
         type: 'text',
       },
+      description: {
+        title: 'Name',
+        type: 'text',
+      },
     },
   };
 
